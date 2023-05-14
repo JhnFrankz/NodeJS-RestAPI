e50de70b779b9f1f5aa3525d1ae743f819bae1bb
Jhan Frank <jhnfrankz@gmail.com>
2023-05-14 13:13:54 -0500

    Agregada conexion a la base de datos MySQL

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 import express from 'express';
+// se usa el .js porque se esta usando el type: module
+import { pool } from './db.js';
 
 const app = express();
+
+/* async: la función que va a esperar a que se resuelva la promesa
+await: será la promesa que se va a esperar */
+
+app.get('/ping', async (req, res) => {
+    // aqui se hace la consulta a la base de datos
+    const [result] = await pool.query('SELECT "Pong" AS result');
+    res.json(result[0]);
+});
 
 app.get('/employees', (req, res) => res.send('Obteniendo lista de empleados'));
 app.post('/employees', (req, res) => res.send('Creando un empleado'));
