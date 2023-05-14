ca15f5fed8a6d5e9672b7db18863f2c61e01ce6a
Jhan Frank <jhnfrankz@gmail.com>
2023-05-14 15:08:13 -0500

    Cambio de rutas y GET agregado

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.json());
 
 app.use(indexRoutes);
-app.use(employeesRoutes);
+app.use('/api', employeesRoutes);
 
 app.listen(3000);
 console.log('Server on port', 3000)
