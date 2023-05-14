9bd6dd7487f01380db353318ba178b664e616897
Jhan Frank <jhnfrankz@gmail.com>
2023-05-14 12:43:48 -0500

    Agregado las rutas basicas o endpoints

--- index.js
+++ index.js
@@ -2,5 +2,10 @@ import express from 'express';
 
 const app = express();
 
+app.get('/employees', (req, res) => res.send('Obteniendo lista de empleados'));
+app.post('/employees', (req, res) => res.send('Creando un empleado'));
+app.put('/employees', (req, res) => res.send('Actualizando un empleado'));
+app.delete('/employees', (req, res) => res.send('Eliminando un empleado'));
+
 app.listen(3000);
 console.log('Server on port', 3000)
